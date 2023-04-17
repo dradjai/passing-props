c8101406a6c6d8581cea91f0c65bffd5d89eb96a
David Radjai <dradjai@yahoo.com>
2023-04-17 10:56:15 -0400

    passing props

---
+++ src/components/GreetingCard.js
@@ -0,0 +1,10 @@
+
+export default function GreetingCard({firstName, age}) {
+  //const {firstName} = props
+
+  return (
+    <div className="GreetingCard">
+      <h3>Hello {firstName} {age}!</h3>
+    </div>
+  )
+}
